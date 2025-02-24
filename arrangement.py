9f1968614ea18d683364bb45c963b3ac9826b62b
Vsreelekha <sreelekha412073@gmail.com>
2025-02-24 21:38:35 +0530

    updated

--- arrangement.py
+++ arrangement.py
@@ -2,12 +2,17 @@ girls=int(input())
 boys=int(input())
 g=list(map(int,input().split()))
 b=list(map(int,input().split()))
-list1=list(zip(g,b))
+g.sort()
+b.sort()
+if g[0]<b[0]:
+    list1=list(zip(g,b))
+else:
+    list1=list(zip(b,g))
 list2=[]
 for i in list1:
     list2.extend(list(i))
 print(list2)
-if sorted(list2):
+if list2==sorted(list2):
     print('Yes')
 else:
     print('No')
